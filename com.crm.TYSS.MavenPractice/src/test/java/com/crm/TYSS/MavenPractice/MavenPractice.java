c85c4caccb161c33ac1e8e69219e44414807212f
Meghna <Meghna@DESKTOP-CKLIH53>
2021-11-11 12:32:57 +0530

    User 1(SDET 23)

---
+++ com.crm.TYSS.MavenPractice/src/test/java/com/crm/TYSS/MavenPractice/MavenPractice.java
@@ -0,0 +1,14 @@
+package com.crm.TYSS.MavenPractice;
+
+import org.testng.annotations.Test;
+
+public class MavenPractice 
+{
+	@Test
+	public void mavenCmd()
+	{
+		System.out.println("Running maven commands!!!");
+	}
+
+
+}
---
+++ com.crm.TYSS.MavenPractice/src/test/java/com/crm/TYSS/MavenPractice/MavenSample2.java
@@ -0,0 +1,13 @@
+package com.crm.TYSS.MavenPractice;
+
+import org.testng.annotations.Test;
+
+public class MavenSample2
+{
+	@Test
+	public void sample2()
+	{
+		System.out.println("batch suite is running!!");
+	}
+
+}
