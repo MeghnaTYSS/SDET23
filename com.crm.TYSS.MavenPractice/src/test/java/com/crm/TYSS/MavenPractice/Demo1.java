5fa0f8293a0f68e200879f4abccfac012ee71626
Meghna <Meghna@DESKTOP-CKLIH53>
2021-11-11 14:19:00 +0530

    Second sample of push and pull

---
+++ com.crm.TYSS.MavenPractice/src/test/java/com/crm/TYSS/MavenPractice/Demo1.java
@@ -0,0 +1,13 @@
+package com.crm.TYSS.MavenPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+	@Test
+	public void demo()
+	{
+		System.out.println("Demo 1 of pull.");
+	}
+
+}
